dcc2ee45ab2c1a3a4b48e93b25f2ee5735a7092f
Eduardo Baeza <eduardo.baeza@yahoo.com>
2021-02-18 21:48:04 -0600

    Menu side bar layout update

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,19 +1,57 @@
-const menuSection = () => {
-    const div1 = document.createElement('div');
-    div1.className = 'flex justify-center items-center text-gray-600 text-base font-medium';
-    const div2 = document.createElement('div');
-    div2.className = 'w-3.5';
-    div2.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" /></svg>';
-    
-    const div3 = document.createElement('div');
-    div3.textContent = 'Projects'
-    
-    const addBtn = document.createElement('button');
-    addBtn.className = 'rounded-full w-4 h-4 bg-blue-500 p-0 border-0 inline-flex items-center justify-center text-white ml-4';
-    addBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>'
-    addBtn.setAttribute('id', 'addProject');
+// import { list } from "postcss";
+import down from '../img/chevron-down.png';
+import plus from '../img/plus.png';
+import bullet from '../img/bullet.png';
 
-    div1.append(div2, div3, addBtn);
+const menuSection = () => {
+    // const div1 = document.createElement('div');
+    // div1.className = 'flex justify-center items-center text-gray-600 text-base font-medium';
+    // const div2 = document.createElement('div');
+    // div2.className = 'w-3.5';
+    // div2.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" /></svg>';
+    
+    // const div3 = document.createElement('div');
+    // div3.textContent = 'Projects'
+    
+    // const addBtn = document.createElement('button');
+    // addBtn.className = 'rounded-full w-4 h-4 bg-blue-500 p-0 border-0 inline-flex items-center justify-center text-white ml-4';
+    // addBtn.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" /></svg>'
+    // addBtn.setAttribute('id', 'addProject');
+
+    // div1.append(div2, div3, addBtn);
+  
+
+    const div1 = document.createElement('div');
+    div1.className = 'antialiased bg-gray-200 min-h-screen p-8';
+    const div2 = document.createElement('div');
+    div2.className = 'flex justify-center';
+    const nav = document.createElement('nav');
+    nav.className = 'w-56 relative';
+    const span1 = document.createElement('span');
+    span1.className = 'absolute h-10 w-full bg-white rounded-lg shadow';
+    const ul = document.createElement('ul');
+    ul.className = 'relative';
+    const li = document.createElement('li');
+    li.className = 'mb-5';
+    const addBtn  = document.createElement('button');
+    addBtn.className = 'py-2 px-5 w-full flex items-center justify-between';
+    addBtn.setAttribute('id', 'addProject');
+    const chevronDown = document.createElement('img');
+    chevronDown.className = 'w-3';
+    chevronDown.src = down;
+    const span2 = document.createElement('span');
+    span2.className = 'ml-2 text-sm font-medium';
+    span2.textContent = 'PROJECTS';
+    const add = document.createElement('img');
+    add.className = 'ml-5';
+    add.src = plus;
+
+    div1.append(div2);
+    div2.append(nav);
+    nav.append(span1, ul);
+    ul.append(li);
+    li.append(addBtn);
+    addBtn.append(chevronDown, span2, add);
 
     return { div1, addBtn } ;
 }
--- src/js/projectForm.js
+++ src/js/projectForm.js
@@ -1,44 +1,34 @@
 const addProjectForm = () => {
     const container = document.createElement('div');
-    container.className = 'w-3/12 pt-32';
-    const form = document.createElement('div');
-    
+    container.className = 'container px-8 pt-48 pb-24 mx-auto lg:px-4';
+
+    const form = document.createElement('form');
+    form.className = 'addProjectForm';
+
     const div1 = document.createElement('div');
-    div1.className = 'shadow sm:rounded-md sm:overflow-hidden';
-    const div2 = document.createElement('div');
-    div2.className = 'px-4 py-5 bg-white space-y-6 sm:p-6';
-    const div3 = document.createElement('div');
-    div3.className = 'grid grid-cols-3 gap-6';
-    const div4 = document.createElement('div');
-    div4.className = 'col-span-3 sm:col-span-2';
-    const label = document.createElement('label');
-    label.className = 'block text-sm font-medium text-gray-700';
-    label.textContent = 'Project name';
-    const div5 = document.createElement('div');
-    div5.className = 'mt-1 flex rounded-md shadow-sm';
-    const span = document.createElement('span');
-    span.className = 'inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-150 text-gray-500 text-sm';
+    div1.className = 'relative';
+
     const input = document.createElement('input');
-    input.className = 'border-gray-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm';
-    input.setAttribute('placeholder', 'Project name');
+    input.setAttribute('id', 'projectName');
+    input.setAttribute('placeholder', 'Add project name');
     input.setAttribute('type', 'text');
-    input.setAttribute('id', 'projectName')
-    const div6 = document.createElement('div');
-    div6.className = 'px-4 py-3 text-right sm:px-6 flex justify-between';
+    input.className = 'addProjectInput';
+
     const submitBtn = document.createElement('button');
-    submitBtn.className = 'inline-flex justify-center mr-3 py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
-    submitBtn.textContent = 'Save Project';
+    submitBtn.className = 'bg-blue-600 hover:bg-blue-700 addProjectBtn mb-3';
+    submitBtn.textContent = 'Add Project';
     submitBtn.setAttribute('type', 'button');
     
     const cancelBtn = document.createElement('button');
-    cancelBtn.className = 'inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500';
+    cancelBtn.className = 'bg-red-700 hover:bg-red-800 addProjectBtn';
     cancelBtn.textContent = 'Cancel';
     cancelBtn.setAttribute('type', 'button');
 
 
-    container.append(form, div1, div2, div3, div4, div5, div6);
-    div4.append(label);
-    div6.append(submitBtn, cancelBtn);
+    container.append(form);
+    form.append(div1, submitBtn, cancelBtn);
+    div1.append(input);
+
 
     return { container, submitBtn };
 
